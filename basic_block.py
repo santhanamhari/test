077712fca10cac1c30654c30ce07ab0991bf6c35
santhanamhari <40223805+santhanamhari@users.noreply.github.com>
2026-02-23 15:51:25 -0500

    Implement BasicBlock with 3x3 convolutions

---
+++ basic_block.py
@@ -0,0 +1,45 @@
+# onconet/models/blocks/basic_block.py
+
+import torch.nn as nn
+from onconet.models.blocks.factory import RegisterBlock
+
+
+def conv3x3(in_planes, out_planes, stride=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(
+        in_planes, out_planes, 
+        kernel_size=3, stride=stride, padding=1, bias=False
+    )
+
+
+@RegisterBlock('BasicBlock')
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, args, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
